8a3112729ee75f69f2d87affe1d5ef64f6908350
Antony <vontonnie@gmail.com>
2021-03-23 19:15:45 +0300

    Downgrade Junit to 4.12

--- app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/taskdetail/TaskDetailFragmentTest.kt
+++ app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/taskdetail/TaskDetailFragmentTest.kt
@@ -1,5 +1,30 @@
 package com.example.android.architecture.blueprints.todoapp.taskdetail
 
+import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.MediumTest
+import com.example.android.architecture.blueprints.todoapp.R
+import com.example.android.architecture.blueprints.todoapp.data.Task
 import org.junit.Assert.*
+import org.junit.Test
+import org.junit.runner.RunWith
 
+@MediumTest // signifies Integrate Test -crucial for grouping/filtering tests
+@RunWith(AndroidJUnit4::class)
 class TaskDetailFragmentTest
+
+
+@Test
+fun activeTasks_displayedInUi(){
+    //GIVEN - Add active (incomplete) task to the DB
+    val activeTask = Task("Active Task", "AndroidX Rocks", false)
+
+    //WHEN - Details fragment launched to display task
+    val bundle = TaskDetailFragmentArgs(activeTask.id).toBundle()
+
+    launchFragmentInContainer<TaskDetailFragment>(bundle, R.style.AppTheme)
+
+
+
+
+}
