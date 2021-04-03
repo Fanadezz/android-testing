0b1a1ff3a1868b6428d87acce33b627021135855
Antony <vontonnie@gmail.com>
2021-04-03 18:06:40 +0300

    Add Rules to StatisticsViewModelTest

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsViewModelTest.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsViewModelTest.kt
@@ -1,5 +1,22 @@
 package com.example.android.architecture.blueprints.todoapp.statistics
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.example.android.architecture.blueprints.todoapp.MainCoroutineRule
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.junit.Assert.*
+import org.junit.Rule
+@ExperimentalCoroutinesApi
+class StatisticsViewModelTest{
 
-class StatisticsViewModelTest
+
+    /*since we are testing Architecture Component we get InstantTaskExecutorRule*/
+    @get:Rule
+    val instantExecutorRule = InstantTaskExecutorRule()
+
+
+    /*Since we are testing Coroutines and View Model we get mainCoroutineRule*/
+
+
+    @get:Rule
+    var mainCoroutineRule = MainCoroutineRule()
+}
