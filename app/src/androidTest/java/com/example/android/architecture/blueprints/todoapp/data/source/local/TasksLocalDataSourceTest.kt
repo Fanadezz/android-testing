480c1d0d2e366aee12fa1a6138437820e63b6c48
Antony <vontonnie@gmail.com>
2021-04-04 16:41:20 +0300

    Add TasksActivityTest file

--- app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/data/source/local/TasksLocalDataSourceTest.kt
+++ app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/data/source/local/TasksLocalDataSourceTest.kt
@@ -5,12 +5,20 @@ import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider.getApplicationContext
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
+import com.example.android.architecture.blueprints.todoapp.data.Result
+import com.example.android.architecture.blueprints.todoapp.data.Task
+import com.example.android.architecture.blueprints.todoapp.data.succeeded
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.runBlockingTest
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.CoreMatchers.notNullValue
 import org.junit.After
 import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
 
 @ExperimentalCoroutinesApi
@@ -46,5 +54,52 @@ class TasksLocalDataSourceTest {
     fun closeDatabase() {
 
         database.close()
+    }
+
+
+    @Test
+    fun saveTask_retrievesTask() = runBlocking{
+
+        //GIVEN - A new task saved in the database
+        val newTask = Task("Title", "Description", false)
+
+        localDataSource.saveTask(newTask)
+
+
+        //WHEN - Task retrieved by ID
+        val result =  localDataSource.getTask(newTask.id)
+
+
+        //THEN - Same task is returned
+        assertThat(result.succeeded, `is`(true))
+
+
+result as Result.Success
+        assertThat(result.data.description, `is` ("Description"))
+        assertThat(result.data.title, `is` ("Title"))
+        assertThat(result.data.isCompleted, `is` (false))
+    }
+
+    @Test
+    fun completedTask_retrievedTaskIsComplete() = runBlocking{
+
+        //GIVEN - Save a new active task in the local data source
+
+        //isCompleted is false so the task is activeTask
+val task = Task("Title", "Description", false)
+        localDataSource.saveTask(task)
+
+
+        //WHEN - Mark it as complete
+        localDataSource.completeTask(task)
+
+        //THEN - Check that the task can be retrieved from the local data source and is complete
+
+        val loadedTask = localDataSource.getTask(task.id)
+
+
+        assertThat(loadedTask.succeeded, `is`(true))
+        loadedTask as Result.Success
+        assertThat(loadedTask.data.isCompleted, `is`(true))
     }
 }
