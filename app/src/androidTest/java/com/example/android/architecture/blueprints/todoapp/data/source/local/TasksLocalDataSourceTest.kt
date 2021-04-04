e6204131ab5605aef5282b49baa0424461b15ae5
Antony <vontonnie@gmail.com>
2021-04-04 15:07:21 +0300

    SetUp TasksLocalDataSourceTest Skelton

--- app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/data/source/local/TasksLocalDataSourceTest.kt
+++ app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/data/source/local/TasksLocalDataSourceTest.kt
@@ -1,15 +1,50 @@
 package com.example.android.architecture.blueprints.todoapp.data.source.local
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider.getApplicationContext
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.MediumTest
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.After
 import org.junit.Assert.*
+import org.junit.Before
 import org.junit.Rule
+import org.junit.runner.RunWith
 
+@ExperimentalCoroutinesApi
+@RunWith(AndroidJUnit4::class)
+@MediumTest
 
-
-class TasksLocalDataSourceTest{
+class TasksLocalDataSourceTest {
 
 
     /*Executes each task synchronously using Architecture Components*/
     @get:Rule
     var instantExecutorRule = InstantTaskExecutorRule()
+
+
+    //subjects under test
+    private lateinit var localDataSource: TasksLocalDataSource
+    private lateinit var database: ToDoDatabase
+
+
+    @Before
+    fun initDatabase() {
+
+        database = Room.inMemoryDatabaseBuilder(getApplicationContext(), ToDoDatabase::class
+                .java)
+                .allowMainThreadQueries()
+                .build()
+
+        localDataSource = TasksLocalDataSource(database.taskDao(), Dispatchers.Main)
+    }
+
+
+    @After
+    fun closeDatabase() {
+
+        database.close()
+    }
 }
