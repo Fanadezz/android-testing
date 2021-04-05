788e2775ec7b4deace461f06758635c0c0e442c6
Antony <vontonnie@gmail.com>
2021-04-05 10:03:14 +0300

    Test Deleting Tasks

--- app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/TasksActivityTest.kt
+++ app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/TasksActivityTest.kt
@@ -16,6 +16,7 @@ import com.example.android.architecture.blueprints.todoapp.data.source.ITasksRep
 import com.example.android.architecture.blueprints.todoapp.tasks.TasksActivity
 import com.example.android.architecture.blueprints.todoapp.util.DataBindingIdlingResource
 import com.example.android.architecture.blueprints.todoapp.util.EspressoIdlingResource
+import com.example.android.architecture.blueprints.todoapp.util.monitorActivity
 import kotlinx.coroutines.runBlocking
 import org.hamcrest.CoreMatchers.not
 import org.junit.After
@@ -76,7 +77,8 @@ class TasksActivityTest {
         //Start up Tasks screen
         val activityScenario = ActivityScenario.launch(TasksActivity::class.java)
 
-        //
+        //monitor activity for pending data binding
+        dataBindingIdlingResource.monitorActivity(activityScenario)
 
         //Click on the task on the list and verify that all data is correct
         onView(withText("Title 1")).perform(click())
@@ -85,7 +87,7 @@ class TasksActivityTest {
         onView(withId(R.id.task_detail_description_text)).check(matches(withText("Description")))
         onView(withId(R.id.task_detail_complete_checkbox)).check(matches(not(isChecked())))
 
-        //Click on the etit button, edit and save
+        //Click on the edit button, edit and save
         onView(withId(R.id.edit_task_fab)).perform(click())
         onView(withId(R.id.add_task_title_edit_text)).perform(replaceText("New Title"))
         onView(withId(R.id.add_task_description_edit_text)).perform(replaceText("New Description"))
@@ -104,5 +106,36 @@ class TasksActivityTest {
         //Ensure the activity is closed before resetting the database
         activityScenario.close()
     }
+
+    @Test
+    fun createOneTask_deleteTask() {
+
+        // 1. Start TasksActivity.
+
+        val activityScenario = ActivityScenario.launch(TasksActivity::class.java)
+        //monitor activity for pending data binding
+        dataBindingIdlingResource.monitorActivity(activityScenario)
+        // 2. Add an active task by clicking on the FAB and saving a new task.
+
+        onView(withId(R.id.add_task_fab)).perform(click())
+        onView(withId(R.id.add_task_title_edit_text)).perform(replaceText("Title 1"))
+        onView(withId(R.id.add_task_description_edit_text)).perform(replaceText("Description"))
+        onView(withId(R.id.save_task_fab)).perform(click())
+
+        // 3. Open the new task in a details view.
+        onView(withText("Title 1")).perform(click())
+
+        // 4. Click delete task in menu.
+        onView(withId(R.id.menu_delete)).perform(click())
+
+        // 5. Verify it was deleted.
+        onView(withText("Title 1")).check(doesNotExist())
+
+        // 6. Make sure the activity is closed.
+
+        activityScenario.close()
+
+    }
+
 
 }
